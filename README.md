ba382e18b75722d7b50235d14c40146d208e51d5
Eliel Silva <elielmsilvatec@gmail.com>
2025-03-20 20:36:08 -0400

    atualização do indicador Fear and Greed Index

--- README.md
+++ README.md
@@ -1,32 +1,72 @@
-# binance-futures
-Simple trader bot for Binance Futures.
+# Robô Trader Automatizado - Binance Futures
+
+Este é um robô trader automatizado desenvolvido para operar na plataforma **Binance Futures**. Ele utiliza estratégias baseadas em indicadores técnicos como **SAR (Parabolic SAR)**, **EMA (Média Móvel Exponencial)** e o **Índice de Medo e Ganância (Fear and Greed Index)** para tomar decisões de compra e venda. O robô foi desenvolvido em **Node.js** e se conecta à API da Binance para executar operações em tempo real.
+
+---
 
 ## AVISOS
 
-- VOCÊ DEVE TER CONHECIMENTOS BÁSICOS DE LÓGICA DE PROGRAMAÇÃO E DE ALGORITMOS PARA USAR ESTE ROBÔ.
-- EU NÃO ME RESPONSABILIZO PELO USO INDEVIDO DESTE ROBÔ TRADER, BUGS QUE ELE POSSA TER OU A LÓGICA DE TRADING QUE VOCÊ VENHA A APLICAR.
-- EU NÃO ME RESPONSABILIZO POR PERDAS FINANCEIRAS E NÃO DOU CONSELHOS DE INVESTIMENTO.
-- CRIPTOMOEDAS É INVESTIMENTO DE RISCO, TENHA ISSO EM MENTE.
-- NÃO COMPARTILHE SUAS VARIÁVEIS DE AMBIENTE E ARQUIVO .ENV COM NINGUÉM, NEM COMIGO.
-- AO USAR ESTE ROBÔ, VOCÊ ASSUME QUALQUER RISCO FINANCEIRO QUE ELE POSSA LHE CAUSAR.
-- NÃO DESENVOLVO PARA TERCEIROS
+- **VOCÊ DEVE TER CONHECIMENTOS BÁSICOS DE LÓGICA DE PROGRAMAÇÃO E DE ALGORITMOS PARA USAR ESTE ROBÔ.**
+- **EU NÃO ME RESPONSABILIZO PELO USO INDEVIDO DESTE ROBÔ TRADER, BUGS QUE ELE POSSA TER OU A LÓGICA DE TRADING QUE VOCÊ VENHA A APLICAR.**
+- **EU NÃO ME RESPONSABILIZO POR PERDAS FINANCEIRAS E NÃO DOU CONSELHOS DE INVESTIMENTO.**
+- **CRIPTOMOEDAS É INVESTIMENTO DE RISCO, TENHA ISSO EM MENTE.**
+- **NÃO COMPARTILHE SUAS VARIÁVEIS DE AMBIENTE E ARQUIVO `.ENV` COM NINGUÉM, NEM COMIGO.**
+- **AO USAR ESTE ROBÔ, VOCÊ ASSUME QUALQUER RISCO FINANCEIRO QUE ELE POSSA LHE CAUSAR.**
+- **NÃO DESENVOLVO PARA TERCEIROS.**
 
-## Instruções
+---
 
-1. Execute npm install na pasta do projeto que vai executar para baixar dependências
-2. Obtenha suas credenciais de API na sua área logada da sua exchange
-3. Crie um arquivo .env na raiz do projeto com o mesmo conteúdo do arquivo .env.example
-4. Preencha os valores das variáveis no .env conforme instruído nos comentários do .env.example
-5. Execute o robô com npm start, leva 1 segundo para começar a monitorar
+## Funcionalidades
 
-## Mais informações
+- **Operações Automatizadas**: Compra e venda de contratos futuros na Binance.
+- **Indicadores Técnicos**:
+  - **SAR (Parabolic SAR)**: Identifica pontos de reversão de tendência.
+  - **EMA (Média Móvel Exponencial)**: Ajuda a identificar tendências de mercado.
+  - **Fear and Greed Index**: Avalia o sentimento do mercado (medo ou ganância).
+- **Gerenciamento de Risco**: Alavancagem configurável e controle de margem inicial.
+- **WebSocket**: Conexão em tempo real com a Binance para receber atualizações de preço.
 
-Crie suas chaves de API para Futuros com este vídeo: https://www.youtube.com/watch?v=FCzgqr2P0_g
+---
 
-Tutorial em breve no blog, assine a newsletter https://www.luiztools.com.br
+## Tecnologias Utilizadas
 
-Conheça meu curso de bot para criptomoedas: https://www.luiztools.com.br/curso-beholder
+- **Node.js**: Ambiente de execução JavaScript.
+- **Binance API**: Integração com a API da Binance para operações de trading.
+- **WebSocket**: Conexão em tempo real com o stream de preços da Binance.
+- **Axios**: Requisições HTTP para buscar dados externos (Fear and Greed Index).
+- **dotenv**: Gerenciamento de variáveis de ambiente.
+- **Indicadores Técnicos**: Implementação manual de SAR e EMA.
 
-Conheça meus livros: https://www.luiztools.com.br/meus-livros
+---
 
-Me siga nas redes sociais: https://about.me/luiztools
+## Como Configurar e Executar
+
+### Pré-requisitos
+- Node.js instalado (versão 16 ou superior).
+- Conta na Binance com API Key e Secret Key geradas.
+- Conhecimento básico de trading e criptomoedas.
+
+### Passos para Configuração
+
+1. **Clone o repositório**:
+   ```bash
+   git clone https://github.com/elielmsilvatec/conect-binance-futures
+   cd conect-binance-futures
+
+### Instale as dependências:
+   npm install
+
+### Instale as dependências:
+API_KEY=sua_api_key_aqui
+SECRET_KEY=sua_secret_key_aqui
+API_URL=https://testnet.binancefuture.com/fapi
+STREAM_URL=wss://fstream.binance.com/ws/
+
+/robo-trader-binance
+│
+├── /node_modules          # Dependências do projeto
+├── /api                   # Integração com a API da Binance
+├── index.js               # Arquivo principal do robô
+├── .env                   # Variáveis de ambiente
+├── package.json           # Dependências e scripts
+└── README.md              # Este arquivo
--- index.js
+++ index.js
@@ -1,33 +1,169 @@
-const ethRoutes = require('./eth');
+require("dotenv").config();
+const WebSocket = require("ws");
+const axios = require("axios");
+const api = require("./api");
 
+// const Medias = require("./medias");
 
-const express = require('express');
-const app = express();
-// const Database = require('./database/FuturosModel')
+/////////////////////////////////////////////////////////////////////
+const TimeFrame = "1h";
+const alavancagem = 2;
+const symbol = "ETHUSDT";
+const valor_entrada = 0.001;
 
-// Configurar o mecanismo de visualização
-app.set('view engine', 'ejs');  // Use EJS como mecanismo de visualização
-app.set('views', __dirname + '/views');  // Especifique o diretório de views (caso não esteja na raiz)
+const AF_INITIAL = 0.02; // Fator de aceleração inicial
+const AF_MAX = 0.2; // Limite do fator de aceleração
+let SAR = 0; // Valor inicial do SAR
+let EP = 0; // Ponto extremo
+let AF = AF_INITIAL; // Fator de aceleração
 
+// Variável para armazenar a EMA anterior
+let previousEMA = 0;
 
-// Rota padrão para '/'
-
-app.get('/', async (req, res) => {
-
-    try {
-         
-      const futuros = await Database.findAll();
-      res.render('app', {
-      futuros
-      })
-  } catch (error) {
-      res.status(500).json({ error: error.message });
+const ws = new WebSocket(
+  `${process.env.STREAM_URL}${symbol.toLowerCase()}@markPrice@1s`,
+  {
+    handshakeTimeout: 60000, // Tempo limite de aperto de mão em milissegundos
   }
-   
-  })
-  
+);
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+let Operacao_aberta = 0;
+
+//*************************************************************************************** */
+// usar essa api apenas para consulta da moeda
+const Binance = require("binance-api-node");
+const binance = new Binance.default({
+  apiKey: process.env.API_KEY,
+  apiSecret: process.env.SECRET_KEY,
 });
+//*************************************************************************************** */
+// Função para calcular o SAR
+function calcularSAR(candle) {
+  // Determinar se estamos em uma tendência de alta ou baixa
+  if (candle.close > SAR) {
+    // Tendência de alta
+    EP = Math.max(candle.high, EP); // O ponto extremo será a máxima do candle
+  } else {
+    // Tendência de baixa
+    EP = Math.min(candle.low, EP); // O ponto extremo será a mínima do candle
+  }
+
+  // Calcular o próximo SAR
+  SAR = SAR + AF * (EP - SAR);
+
+  // Ajustar o fator de aceleração
+  if (EP === candle.high) {
+    AF = Math.min(AF + 0.02, AF_MAX); // Aumenta o AF em 0.02 até o limite máximo
+  } else if (EP === candle.low) {
+    AF = Math.min(AF + 0.02, AF_MAX); // Aumenta o AF em 0.02 até o limite máximo
+  }
+
+  return SAR;
+}
+
+// Função para calcular a EMA de 20 períodos
+function calcularEMA(candles, periodo = 20) {
+  // O preço de fechamento do último candle
+  let closePrice = parseFloat(candles[candles.length - 1].close);
+
+  // Calculando o fator de suavização K
+  const K = 2 / (periodo + 1);
+
+  // Se não temos uma EMA anterior (caso seja o primeiro cálculo), inicializamos com o fechamento
+  if (previousEMA === 0) {
+    previousEMA = closePrice;
+  }
+
+  // Calcula a EMA atual
+  let EMA = closePrice * K + previousEMA * (1 - K);
+
+  // Atualiza o valor da EMA anterior para o próximo cálculo
+  previousEMA = EMA;
+
+  return EMA;
+}
+
+// Função para buscar o Fear and Greed Index
+async function obterFearAndGreedIndex() {
+    try {
+      const response = await axios.get("https://api.alternative.me/fng/");
+      const data = response.data;
+  
+      const indexAtual = data.data[0];
+      console.log(`Fear and Greed Index: ${indexAtual.value} (${indexAtual.value_classification})`);
+      return {
+        valor: parseInt(indexAtual.value),
+        classificacao: indexAtual.value_classification,
+      };
+    } catch (error) {
+      console.error("Erro ao buscar o Fear and Greed Index:", error);
+      return null;
+    }
+  }
+  
+ws.onmessage = async (event) => {
+  try {
+    let obj = JSON.parse(event.data);
+    const price = parseFloat(obj.p);
+
+    // dados da minha conta
+    let dados_conta = await api.accountInfo();
+    let initialMargin = parseFloat(dados_conta.totalInitialMargin);
+
+    let simbulo = symbol;
+    let candles = await binance.futuresCandles({
+      symbol: simbulo,
+      interval: TimeFrame,
+      limit: 2, // Pegue os últimos dois candles
+    });
+
+
+    // Pegue o último candle para calcular o SAR
+    let vl = candles[1]; // O segundo último candle (o último válido para o cálculo)
+    let lastHigh = parseFloat(vl.high);
+    let lastLow = parseFloat(vl.low);
+    let lastClose = parseFloat(vl.close);
+
+    let aux = candles[candles.length - 2];
+    let ultima_vela = parseFloat(aux.close);
+
+    // Calcular o SAR
+    let novoSAR = calcularSAR({
+      high: lastHigh,
+      low: lastLow,
+      close: lastClose,
+    });
+    // Calcular a EMA de 20 períodos
+    let EMA20 = calcularEMA(candles);
+
+
+
+    // Adicione o Fear and Greed Index
+    const fng = await obterFearAndGreedIndex();
+
+    if (fng) {
+      console.log(`Fear and Greed Index atual: ${fng.valor} (${fng.classificacao})`);
+
+      // Lógica com base no Fear and Greed Index
+      if (fng.valor < 25) {
+        console.log("Medo extremo detectado. Avaliando compra...");
+        // Lógica para compra
+      } else if (fng.valor > 75) {
+        console.log("Ganância extrema detectada. Avaliando venda...");
+        // Lógica para venda
+      } else {
+        console.log("Mercado neutro. Monitorando...");
+      }
+    }
+    
+
+  } catch (error) {
+    console.log("Erro index: ", error);
+  }
+};
+
+// Informações da conta
+// setInterval(() => {
+//     api.accountInfo()
+//         .catch(err => console.error(err));
+// }, 10000)
